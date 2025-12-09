32448bc037dba5e976aad5fe3a6354d83a52e1c0
zarkfire <mc.carruette@gmail.com>
2025-12-09 12:48:33 +0100

    fix : debug enlevé

--- php/messagerie.php
+++ php/messagerie.php
@@ -3,7 +3,6 @@
 
 <?php include "db2.php"; ?>
 <?php
-echo $userId;
 // Traiter l'envoi de message en PREMIER
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['message'])) {
   ob_clean(); // Nettoyer tout buffer
