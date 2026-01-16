5496d3447d8b964ede6f9ff928ae64688418aee0
Pharell <rellpha0@gmail.com>
2026-01-16 10:54:13 +0100

    mvc faq

--- MVC/Model/FAQmodel.php
+++ MVC/Model/FAQmodel.php
@@ -1,20 +1,7 @@
 <?php
 
 class FAQModel {
-
-private $pdo;
-	public function __construct($pdo) {
-		$this->pdo = $pdo;
-	}
     public function getFAQ() {
-        try {
-            $query = "SELECT question, reponse FROM faq ORDER BY id_faq";
-            $stmt = $this->pdo->prepare($query);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch(PDOException $e) {
-            echo "Erreur de récupération des FAQ : " . $e->getMessage();
-            return [];
-        }
-    }
+        return "FAQ content";
+    }    
 }
