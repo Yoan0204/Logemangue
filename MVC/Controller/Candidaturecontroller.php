5d0efea145102cab20334fb092df5d4f00eb9577
Pharell <rellpha0@gmail.com>
2026-01-16 10:06:49 +0100

    mvc candidature

--- MVC/Controller/Candidaturecontroller.php
+++ MVC/Controller/Candidaturecontroller.php
@@ -1,14 +1,14 @@
 <?php
 
 class Candidaturecontroller {
-    private $Candidaturemodel;
+    private $model;
 
-    public function __construct($Candidaturemodel) {
-        $this->Candidaturemodel = $Candidaturemodel;
+    public function __construct($model) {
+        $this->model = $model;
     }
 
     public function showCandidatures($userId) {
-        $candidatures = $this->Candidaturemodel->getCandidatures($userId);
+        $candidatures = $this->model->getCandidatures($userId);
         require_once __DIR__ . '/../View/Candidatureview.php';
         $view = new CandidatureView();
         $view->render($candidatures);
